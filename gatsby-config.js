c819d9f11b651405b0c85c3ec86ea6475d1b5180
ianHansborough <ian.hansborough@gmail.com>
2018-10-12 10:39:13 -0400

    cleaned up global styles and manifest params

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,11 +22,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
+        name: "ianhansborough.com",
+        short_name: "ianhansborough.com",
         start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
+        background_color: "#fff",
+        theme_color: "#fff",
         display: "minimal-ui",
         icon: "src/images/personal-site-logo-alt.svg", // This path is relative to the root of the site.
       },
